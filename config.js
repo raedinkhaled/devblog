17857dde1c71349b3f64d58f23fad916a4301bd7
Raedin Khaled Sakhri <ksakhri999@gmail.com>
2020-03-22 21:02:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = {
   title: 'أخبار الكوفيد 19 في الجزائر', // Required
   author: 'أخبار الكوفيد 19 في الجزائر', // Required
   primaryColor: '#3498db', // Required
-  showHeaderImage: true,
+  showHeaderImage: false,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
     facebook: 'https://www.facebook.com/raedin.khaled',
   },
   pathPrefix: '/CoronaNews',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://news.covid19-algeria.com',
 };
